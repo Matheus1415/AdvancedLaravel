a5f696f81d8bee90835e420e9c9846cb1cf521c9
Matheus <mps.web.tech@gmail.com>
2025-06-15 16:31:28 -0300

    Blades - 15/06/2025

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,27 @@
-{{--Extende o html que for chamado--}}
+{{-- Extende o html que for chamado --}}
 @extends('layout')
 
-@section('styled')
-<link rel="stylesheet" href="home.css">
+@section('script')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
 @endsection
 
-{{--Chama o yield "Lugar onde serar renderizado pelo nome dado a ele"--}}
+@section('styled')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@endsection
+
+{{-- Chama o yield "Lugar onde serar renderizado pelo nome dado a ele" --}}
 @section('content')
-<h2>Home</h2>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti suscipit autem iste minus? Voluptate minima itaque voluptatum sapiente natus. Repudiandae quo, sed ut eaque enim dolorem nostrum quibusdam inventore rerum.</p>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti suscipit autem iste minus? Voluptate minima itaque
+        voluptatum sapiente natus. Repudiandae quo, sed ut eaque enim dolorem nostrum quibusdam inventore rerum.</p>
+    <button class="btn btn-warning btn-sm">Clicar</button>
+    <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    
     @yield('styled')
 </head>
 <body>
@@ -12,5 +13,6 @@
         {{--o html que herdar vai ser renderizado abaixo--}}
         @yield('content')
     </div>
+    @yield('script')
 </body>
 </html>
