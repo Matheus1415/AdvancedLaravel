e6b8ba66db79e6dd9a981ba10a9d3fb71e2acb8f
Matheus <mps.web.tech@gmail.com>
2025-08-18 16:22:26 -0300

    GOOGLE - First commit

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginGoogleController;
 use App\Jobs\PaymentJob;
 use App\Jobs\TesteJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [HomeController::class, 'index'])->name('home');
-Route::get("/about", [AboutController::class, 'index'])->name('home');
+Route::get("/login", [LoginGoogleController::class, 'index'])->name('home');
 Route::get('/payment', function () {
     $user = Auth::user();
     PaymentJob::dispatch($user)->delay(now()->addSeconds(5))->onQueue('payments');
