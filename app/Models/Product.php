50bc31cc7d35e8a225f9ce3cfd6c56cca20823e1
Matheus <mps.web.tech@gmail.com>
2025-10-08 17:55:57 -0300

    fix: Laravel ORM

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'product_name',
+        'price',
+    ];
+
+    public function clients(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Client::class,
+            'orders',
+            'product_id',
+            'client_id'
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,46 @@ Route::get('/payment', function () {
     PaymentJob::dispatch($user)->delay(now()->addSeconds(5))->onQueue('payments');
     TesteJob::dispatch();
 });
+
+Route::get('/product', function () {
+    $product = App\Models\Product::all();
+    $product_max = App\Models\Product::max('price');
+    $product_min = App\Models\Product::mim('price');
+    $product_sum = App\Models\Product::sum('price');
+    $product_media = App\Models\Product::avg('price');
+
+    return response()->json($product_media);
+});
+
+Route::get('/client', function () {
+    // $client = App\Models\Client::find(1)->phone;
+    // $client = App\Models\Client::with('phone')->find(1);
+    $client = App\Models\Client::with('phones')->find(1);
+
+    return response()->json($client);
+});
+
+Route::get('/query', function () {
+    // $client = App\Models\Client::with('phones')->find(1);
+    // $phones = $client->phones()->where('phone_number', 'like', '8%')->get();
+    // return response()->json($phones);
+
+    $client = App\Models\Client::find(1);
+    $products = $client->products()
+        ->where('price', '>', 50)
+        ->distinct()
+        ->orderBy('product_name', 'ASC')
+        ->get();
+
+    return response()->json($products);
+});
+
+Route::get('/many', function () {
+    // $client = App\Models\Client::find(1);
+    // $products = $client->products;
+
+    $product = App\Models\Product::find(1);
+    $clients = $product->clients;
+
+    return response()->json($clients);
+});
